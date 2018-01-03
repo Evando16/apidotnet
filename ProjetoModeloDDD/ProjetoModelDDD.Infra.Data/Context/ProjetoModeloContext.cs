00367db85e8e9f015637b7b3758c4cbb19903d46
Evando Junior <evando@ciandt.com>
2018-01-03 20:22:16 -0200

    Start

---
+++ ProjetoModeloDDD/ProjetoModelDDD.Infra.Data/Context/ProjetoModeloContext.cs
@@ -0,0 +1,16 @@
+﻿using ProjetoModelDDD.Domain.Entities;
+using System.Data.Entity;
+
+namespace ProjetoModelDDD.Infra.Data.Context
+{
+    public class ProjetoModeloContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public ProjetoModeloContext()
+        : base("ProjetoModeloDDD")
+        {
+
+        }
+    }
+}
